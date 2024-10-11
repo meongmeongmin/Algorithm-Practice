48129da0d8b87c7cd6183b9d9772871e2483176f
멍멍민 <pr620718@cu.ac.kr>
2024-10-11 15:53:08 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    #pragma region I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #pragma endregion
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < (n - 1) - i; j++)
+            cout << " ";
+
+        for (int j = 0; j <= i * 2; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j <= i; j++)
+            cout << " ";
+
+        for (int j = 1; j < ((n - 1) - i) * 2; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+}
