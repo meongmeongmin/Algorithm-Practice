bc91813dfa00784a5053e7a9efac6004e462fe29
멍멍민 <pr620718@cu.ac.kr>
2026-03-14 21:09:22 +0900

    [Unrated] Title: 생일 출력하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/2555. 생일 출력하기/생일 출력하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "3/19";
+}
